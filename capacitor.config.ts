2d525492b1bcf69f3bed4a2125d3c1c860448d28
Hana <pilkova.hana@gmail.com>
2022-12-13 21:13:29 +0100

    init of app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.knitIt',
+  appName: 'knitItMobileApp',
+  webDir: 'dist/knit-it-mobile-app',
+  bundledWebRuntime: false
+};
+
+export default config;
